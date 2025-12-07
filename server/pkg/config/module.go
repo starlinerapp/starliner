cd85e0efd04a54a2a56cc36d81099eb6ed7a8e68
Leon Liang <hello@leonliang.lu>
2025-12-07 15:34:50 +0100

    simplify config module

--- server/pkg/config/module.go
+++ server/pkg/config/module.go
@@ -2,11 +2,7 @@ package config
 
 import "go.uber.org/fx"
 
-func ProvideConfig() (*Config, error) {
-	return LoadConfig()
-}
-
 var Module = fx.Module(
 	"config",
-	fx.Provide(ProvideConfig),
+	fx.Provide(LoadConfig),
 )
