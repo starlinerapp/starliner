8546e997c77dc58bfaafdb26e3508d777355ce07
Leon Liang <hello@leonliang.lu>
2025-12-20 12:07:22 +0100

    merge server and worker Dockerfiles

--- server/pkg/proto/v1/project.pb.go
+++ server/pkg/proto/v1/project.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.36.11
 // 	protoc        v5.27.5
-// source: proto/v1/project.proto
+// source: pkg/proto/v1/project.proto
 
 package v1
 
@@ -32,7 +32,7 @@ type Project struct {
 
 func (x *Project) Reset() {
 	*x = Project{}
-	mi := &file_proto_v1_project_proto_msgTypes[0]
+	mi := &file_pkg_proto_v1_project_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -44,7 +44,7 @@ func (x *Project) String() string {
 func (*Project) ProtoMessage() {}
 
 func (x *Project) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_v1_project_proto_msgTypes[0]
+	mi := &file_pkg_proto_v1_project_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +57,7 @@ func (x *Project) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Project.ProtoReflect.Descriptor instead.
 func (*Project) Descriptor() ([]byte, []int) {
-	return file_proto_v1_project_proto_rawDescGZIP(), []int{0}
+	return file_pkg_proto_v1_project_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Project) GetId() int64 {
@@ -81,33 +81,33 @@ func (x *Project) GetOrganization() int64 {
 	return 0
 }
 
-var File_proto_v1_project_proto protoreflect.FileDescriptor
+var File_pkg_proto_v1_project_proto protoreflect.FileDescriptor
 
-const file_proto_v1_project_proto_rawDesc = "" +
+const file_pkg_proto_v1_project_proto_rawDesc = "" +
 	"\n" +
-	"\x16proto/v1/project.proto\x12\bproto.v1\"Q\n" +
+	"\x1apkg/proto/v1/project.proto\x12\bproto.v1\"Q\n" +
 	"\aProject\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\x03R\x02id\x12\x12\n" +
 	"\x04name\x18\x02 \x01(\tR\x04name\x12\"\n" +
 	"\forganization\x18\x03 \x01(\x03R\forganizationB\x18Z\x16starliner.app/proto/v1b\x06proto3"
 
 var (
-	file_proto_v1_project_proto_rawDescOnce sync.Once
-	file_proto_v1_project_proto_rawDescData []byte
+	file_pkg_proto_v1_project_proto_rawDescOnce sync.Once
+	file_pkg_proto_v1_project_proto_rawDescData []byte
 )
 
-func file_proto_v1_project_proto_rawDescGZIP() []byte {
-	file_proto_v1_project_proto_rawDescOnce.Do(func() {
-		file_proto_v1_project_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_proto_v1_project_proto_rawDesc), len(file_proto_v1_project_proto_rawDesc)))
+func file_pkg_proto_v1_project_proto_rawDescGZIP() []byte {
+	file_pkg_proto_v1_project_proto_rawDescOnce.Do(func() {
+		file_pkg_proto_v1_project_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_pkg_proto_v1_project_proto_rawDesc), len(file_pkg_proto_v1_project_proto_rawDesc)))
 	})
-	return file_proto_v1_project_proto_rawDescData
+	return file_pkg_proto_v1_project_proto_rawDescData
 }
 
-var file_proto_v1_project_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_proto_v1_project_proto_goTypes = []any{
+var file_pkg_proto_v1_project_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_pkg_proto_v1_project_proto_goTypes = []any{
 	(*Project)(nil), // 0: proto.v1.Project
 }
-var file_proto_v1_project_proto_depIdxs = []int32{
+var file_pkg_proto_v1_project_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -115,26 +115,26 @@ var file_proto_v1_project_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_proto_v1_project_proto_init() }
-func file_proto_v1_project_proto_init() {
-	if File_proto_v1_project_proto != nil {
+func init() { file_pkg_proto_v1_project_proto_init() }
+func file_pkg_proto_v1_project_proto_init() {
+	if File_pkg_proto_v1_project_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: unsafe.Slice(unsafe.StringData(file_proto_v1_project_proto_rawDesc), len(file_proto_v1_project_proto_rawDesc)),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_pkg_proto_v1_project_proto_rawDesc), len(file_pkg_proto_v1_project_proto_rawDesc)),
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_proto_v1_project_proto_goTypes,
-		DependencyIndexes: file_proto_v1_project_proto_depIdxs,
-		MessageInfos:      file_proto_v1_project_proto_msgTypes,
+		GoTypes:           file_pkg_proto_v1_project_proto_goTypes,
+		DependencyIndexes: file_pkg_proto_v1_project_proto_depIdxs,
+		MessageInfos:      file_pkg_proto_v1_project_proto_msgTypes,
 	}.Build()
-	File_proto_v1_project_proto = out.File
-	file_proto_v1_project_proto_goTypes = nil
-	file_proto_v1_project_proto_depIdxs = nil
+	File_pkg_proto_v1_project_proto = out.File
+	file_pkg_proto_v1_project_proto_goTypes = nil
+	file_pkg_proto_v1_project_proto_depIdxs = nil
 }
