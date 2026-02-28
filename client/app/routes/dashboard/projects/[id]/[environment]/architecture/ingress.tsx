40fd01c516b61ac0f8ecb46f4e2bbfdb1c9f68d1
Leon Liang <hello@leonliang.lu>
2026-02-28 19:26:41 +0100

    remove added host/path

--- client/app/routes/dashboard/projects/[id]/[environment]/architecture/ingress.tsx
+++ client/app/routes/dashboard/projects/[id]/[environment]/architecture/ingress.tsx
@@ -42,7 +42,11 @@ export default function Ingress() {
     mode: "onSubmit",
   });
 
-  const { fields: hostFields, append: appendHost } = useFieldArray({
+  const {
+    fields: hostFields,
+    append: appendHost,
+    remove: removeHost,
+  } = useFieldArray({
     control,
     name: "hosts",
   });
@@ -78,13 +82,15 @@ export default function Ingress() {
       <div className="flex flex-col gap-1">
         <p className="text-sm">Hosts</p>
 
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col">
           {hostFields.map((host, hostIndex) => (
             <div key={host.id}>
               <HostEditor
                 control={control}
                 register={register}
                 hostIndex={hostIndex}
+                showRemove={hostIndex > 0}
+                onRemove={() => removeHost(hostIndex)}
               />
             </div>
           ))}
@@ -121,9 +127,17 @@ interface HostEditorProps {
   control: Control<IngressFormInput>;
   register: UseFormRegister<IngressFormInput>;
   hostIndex: number;
+  showRemove?: boolean;
+  onRemove?: () => void;
 }
 
-function HostEditor({ control, register, hostIndex }: HostEditorProps) {
+function HostEditor({
+  control,
+  register,
+  hostIndex,
+  showRemove,
+  onRemove,
+}: HostEditorProps) {
   const {
     fields: pathFields,
     append: appendPath,
@@ -134,100 +148,120 @@ function HostEditor({ control, register, hostIndex }: HostEditorProps) {
   });
 
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex gap-2">
-        <input
-          className="border-mauve-6 placeholder:text-mauve-11 bg-gray-2 w-full min-w-52 rounded-md border-1 p-2 text-sm"
-          type="text"
-          placeholder="Host*"
-          {...register(`hosts.${hostIndex}.name`)}
-        />
-      </div>
+    <div className="relative">
+      {showRemove && (
+        <button
+          type="button"
+          onClick={onRemove}
+          className="border-mauve-6 text-mauve-7 absolute top-[15px] -left-2 z-10 flex h-4 w-4 cursor-pointer items-center justify-center rounded border-[1.5px] bg-white"
+        >
+          <Minus className="stroke-3" />
+        </button>
+      )}
 
-      <div className="border-mauve-6 relative flex flex-col gap-3 border-l-2 pl-6">
-        {pathFields.map((path, pathIndex) => (
-          <div key={path.id} className="relative flex flex-col gap-1">
-            {pathIndex > 0 && (
-              <button
-                type="button"
-                onClick={() => removePath(pathIndex)}
-                className="border-mauve-6 text-mauve-7 absolute top-4 -left-8 z-10 flex h-4 w-4 cursor-pointer items-center justify-center rounded border-[1.5px] bg-white"
-              >
-                <Minus className="stroke-3" />
-              </button>
-            )}
-            <div className="border-mauve-6 absolute -left-6.5 h-6 w-6 rounded-bl-md border-b-2 border-l-2" />
-            <div className="flex w-full gap-1">
-              <div className="relative min-w-24">
-                <select
-                  className={cn(
-                    "border-mauve-6 bg-gray-2 h-full w-full appearance-none rounded-md border-1 px-2 py-1 text-sm",
-                    "text-mauve-11",
-                  )}
-                  defaultValue={path.pathType ?? ""}
-                  {...register(
-                    `hosts.${hostIndex}.paths.${pathIndex}.pathType`,
-                  )}
+      <div className="border-mauve-6 relative flex flex-col gap-1 border-l-2 pl-6">
+        <div className="flex gap-2">
+          <div className="border-mauve-6 absolute -left-0.5 h-6 w-6 rounded-bl-md border-b-2 border-l-2" />
+          <input
+            className="border-mauve-6 placeholder:text-mauve-11 bg-gray-2 w-full min-w-52 rounded-md border-1 p-2 text-sm"
+            type="text"
+            placeholder="Host*"
+            {...register(`hosts.${hostIndex}.name`)}
+          />
+        </div>
+
+        <div className="border-mauve-6 relative flex flex-col gap-3 border-l-2 pl-6">
+          {pathFields.map((path, pathIndex) => (
+            <div key={path.id} className="relative flex flex-col gap-1">
+              {pathIndex > 0 && (
+                <button
+                  type="button"
+                  onClick={() => removePath(pathIndex)}
+                  className="border-mauve-6 text-mauve-7 absolute top-[15px] -left-8 z-10 flex h-4 w-4 cursor-pointer items-center justify-center rounded border-[1.5px] bg-white"
                 >
-                  <option value="" disabled>
-                    Type*
-                  </option>
-                  {["Prefix", "Exact"].map((pathType) => (
-                    <option key={pathType} value={pathType}>
-                      {pathType}
-                    </option>
-                  ))}
-                </select>
+                  <Minus className="stroke-3" />
+                </button>
+              )}
 
-                <div className="pointer-events-none absolute inset-y-0 right-2 flex items-center">
-                  <ChevronDown width={15} className="stroke-mauve-10" />
+              <div className="border-mauve-6 absolute -left-6.5 h-6 w-6 rounded-bl-md border-b-2 border-l-2" />
+
+              <div className="flex w-full gap-1">
+                <div className="relative min-w-24">
+                  <select
+                    className={cn(
+                      "border-mauve-6 bg-gray-2 h-full w-full appearance-none rounded-md border-1 px-2 py-1 text-sm",
+                      "text-mauve-11",
+                    )}
+                    defaultValue={path.pathType ?? ""}
+                    {...register(
+                      `hosts.${hostIndex}.paths.${pathIndex}.pathType`,
+                    )}
+                  >
+                    <option value="" disabled>
+                      Type*
+                    </option>
+                    {["Prefix", "Exact"].map((pathType) => (
+                      <option key={pathType} value={pathType}>
+                        {pathType}
+                      </option>
+                    ))}
+                  </select>
+
+                  <div className="pointer-events-none absolute inset-y-0 right-2 flex items-center">
+                    <ChevronDown width={15} className="stroke-mauve-10" />
+                  </div>
+                </div>
+
+                <input
+                  className="border-mauve-6 placeholder:text-mauve-11 bg-gray-2 w-full min-w-24 rounded-md border p-2 text-sm"
+                  type="text"
+                  placeholder="Path*"
+                  {...register(`hosts.${hostIndex}.paths.${pathIndex}.path`)}
+                />
+              </div>
+
+              <div className="border-mauve-6 relative ml-1 flex flex-col gap-1 border-l-2 pl-6">
+                <div className="border-mauve-6 absolute -left-0.5 h-6 w-6 rounded-bl-md border-b-2 border-l-2" />
+
+                <div className="relative min-w-48">
+                  <select
+                    className={cn(
+                      "border-mauve-6 bg-gray-2 h-full w-full appearance-none rounded-md border-1 p-2 text-sm",
+                      "text-mauve-11",
+                    )}
+                    defaultValue={path.service ?? ""}
+                    {...register(
+                      `hosts.${hostIndex}.paths.${pathIndex}.service`,
+                    )}
+                  >
+                    <option value="" disabled>
+                      Service*
+                    </option>
+                    {["example-project"].map((svc) => (
+                      <option key={svc} value={svc}>
+                        {svc}
+                      </option>
+                    ))}
+                  </select>
+
+                  <div className="pointer-events-none absolute inset-y-0 right-2 flex items-center">
+                    <ChevronDown width={15} className="stroke-mauve-10" />
+                  </div>
                 </div>
               </div>
-              <input
-                className="border-mauve-6 placeholder:text-mauve-11 bg-gray-2 w-full min-w-24 rounded-md border p-2 text-sm"
-                type="text"
-                placeholder="Path*"
-                {...register(`hosts.${hostIndex}.paths.${pathIndex}.path`)}
-              />
             </div>
-            <div className="border-mauve-6 relative ml-1 flex flex-col gap-1 border-l-2 pl-6">
-              <div className="border-mauve-6 absolute -left-0.5 h-6 w-6 rounded-bl-md border-b-2 border-l-2" />
+          ))}
+        </div>
 
-              <div className="relative min-w-48">
-                <select
-                  className={cn(
-                    "border-mauve-6 bg-gray-2 h-full w-full appearance-none rounded-md border-1 p-2 text-sm",
-                    "text-mauve-11",
-                  )}
-                  defaultValue={path.service ?? ""}
-                  {...register(`hosts.${hostIndex}.paths.${pathIndex}.service`)}
-                >
-                  <option value="" disabled>
-                    Service*
-                  </option>
-                  {["example-project"].map((svc) => (
-                    <option key={svc} value={svc}>
-                      {svc}
-                    </option>
-                  ))}
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-2 flex items-center">
-                  <ChevronDown width={15} className="stroke-mauve-10" />
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+        <Button
+          intent="text"
+          className="p-1 pl-10"
+          type="button"
+          onClick={() => appendPath({ ...emptyPathEntry })}
+        >
+          <Plus className="w-3 stroke-3" /> Add Path
+        </Button>
       </div>
-
-      <Button
-        intent="text"
-        className="pl-10"
-        type="button"
-        onClick={() => appendPath({ ...emptyPathEntry })}
-      >
-        <Plus className="w-3 stroke-3" /> Add Path
-      </Button>
     </div>
   );
 }
