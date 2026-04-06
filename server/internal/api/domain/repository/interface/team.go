6133ff13606f8cfa7cc1cbd93a04d6b4afd660ac
Rafael Neher <rafineher@gmail.com>
2026-04-06 19:08:29 +0200

    optimize team db queries

--- server/internal/api/domain/repository/interface/team.go
+++ server/internal/api/domain/repository/interface/team.go
@@ -8,10 +8,12 @@ import (
 type TeamRepository interface {
 	CreateTeam(ctx context.Context, name string, slug string, organizationID int64) (*entity.Team, error)
 	DeleteTeam(ctx context.Context, id int64) error
+	DeleteTeamIfEmpty(ctx context.Context, id int64) error
 	GetTeamBySlug(ctx context.Context, slug string, organizationID int64) (*entity.Team, error)
 	GetUserTeams(ctx context.Context, organizationID int64, userID int64) ([]*entity.Team, error)
 	GetTeamMembers(ctx context.Context, teamID int64) ([]*entity.User, error)
 	AddTeamMember(ctx context.Context, teamID int64, userID int64) error
 	RemoveTeamMember(ctx context.Context, teamID int64, userID int64) error
 	GetTeamById(ctx context.Context, id int64) (*entity.Team, error)
+	ValidateUserTeamAccess(ctx context.Context, teamID int64, userID int64) error
 }
