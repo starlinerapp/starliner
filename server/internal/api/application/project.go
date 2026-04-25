1bbc4afae5eae749a7d390d04a18243a0b9da101
Leon Liang <hello@leonliang.lu>
2026-04-25 23:59:29 +0200

    #236 delete all EnvironmentDeployments on project delete

--- server/internal/api/application/project.go
+++ server/internal/api/application/project.go
@@ -2,12 +2,18 @@ package application
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"strings"
 
+	"starliner.app/internal/api/domain/entity"
+	"starliner.app/internal/api/domain/port"
 	"starliner.app/internal/api/domain/repository/interface"
 	"starliner.app/internal/api/domain/service"
 	"starliner.app/internal/api/domain/value"
+	corePort "starliner.app/internal/core/domain/port"
 	coreService "starliner.app/internal/core/domain/service"
+	coreValue "starliner.app/internal/core/domain/value"
 )
 
 type ProjectApplication struct {
@@ -16,6 +22,9 @@ type ProjectApplication struct {
 	teamService           *service.TeamService
 	projectRepository     interfaces.ProjectRepository
 	environmentRepository interfaces.EnvironmentRepository
+	deploymentRepository  interfaces.DeploymentRepository
+	queue                 port.Queue
+	crypto                corePort.Crypto
 }
 
 func NewProjectApplication(
@@ -24,6 +33,9 @@ func NewProjectApplication(
 	teamService *service.TeamService,
 	projectRepository interfaces.ProjectRepository,
 	environmentRepository interfaces.EnvironmentRepository,
+	deploymentRepository interfaces.DeploymentRepository,
+	queue port.Queue,
+	crypto corePort.Crypto,
 ) *ProjectApplication {
 	return &ProjectApplication{
 		normalizerService:     normalizerService,
@@ -31,6 +43,9 @@ func NewProjectApplication(
 		teamService:           teamService,
 		projectRepository:     projectRepository,
 		environmentRepository: environmentRepository,
+		deploymentRepository:  deploymentRepository,
+		queue:                 queue,
+		crypto:                crypto,
 	}
 }
 
@@ -71,11 +86,103 @@ func (pa *ProjectApplication) GetProject(ctx context.Context, projectId int64, u
 }
 
 func (pa *ProjectApplication) DeleteProject(ctx context.Context, projectId int64, userId int64) error {
+	envs, err := pa.projectRepository.GetProjectEnvironments(ctx, projectId, userId)
+	if err != nil {
+		return err
+	}
+	for _, env := range envs {
+		if err := pa.deleteEnvironmentDeploymentsFromCluster(ctx, env); err != nil {
+			return err
+		}
+		if err := pa.environmentRepository.DeleteEnvironment(ctx, env.Id); err != nil {
+			return err
+		}
+	}
 	return pa.projectRepository.DeleteProject(ctx, projectId, userId)
 }
 
-func (pa *ProjectApplication) GetProjectCluster(ctx context.Context, projectId int64, userId int64) (*value.ProjectCluster, error) {
+func (pa *ProjectApplication) deleteEnvironmentDeploymentsFromCluster(
+	ctx context.Context,
+	env *entity.Environment,
+) error {
+	ingresses, err := pa.environmentRepository.GetEnvironmentIngressDeployments(ctx, env.Id)
+	if err != nil {
+		return err
+	}
 
+	gitDeployments, err := pa.environmentRepository.GetEnvironmentGitDeployments(ctx, env.Id)
+	if err != nil {
+		return err
+	}
+
+	images, err := pa.environmentRepository.GetEnvironmentImageDeployments(ctx, env.Id)
+	if err != nil {
+		return err
+	}
+
+	databases, err := pa.environmentRepository.GetEnvironmentDatabaseDeployments(ctx, env.Id)
+	if err != nil {
+		return err
+	}
+
+	type projectDeploymentDelete struct {
+		id          int64
+		serviceName string
+	}
+	var deletions []projectDeploymentDelete
+
+	for _, d := range ingresses {
+		deletions = append(deletions, projectDeploymentDelete{d.Id, d.Name})
+	}
+
+	for _, d := range gitDeployments {
+		deletions = append(deletions, projectDeploymentDelete{d.Id, d.Name})
+	}
+
+	for _, d := range images {
+		deletions = append(deletions, projectDeploymentDelete{d.Id, d.ServiceName})
+	}
+
+	for _, d := range databases {
+		deletions = append(deletions, projectDeploymentDelete{d.Id, d.ServiceName})
+	}
+
+	for _, d := range deletions {
+		cluster, err := pa.deploymentRepository.GetDeploymentCluster(ctx, d.id)
+		if err != nil {
+			return err
+		}
+
+		if cluster.Kubeconfig == nil {
+			return fmt.Errorf("cluster kubeconfig is nil")
+		}
+		kubeconfigBase64, err := pa.crypto.Decrypt(*cluster.Kubeconfig)
+		if err != nil {
+			return err
+		}
+
+		normalizedDeploymentName, err := pa.normalizerService.FormatToDNS1123(d.serviceName)
+		if err != nil {
+			return err
+		}
+
+		if err = pa.deploymentRepository.SoftDeleteDeploymentVolume(ctx, d.id); err != nil {
+			return err
+		}
+
+		if err = pa.queue.PublishDeleteDeployment(&coreValue.Deployment{
+			DeploymentId:     d.id,
+			DeploymentName:   normalizedDeploymentName,
+			Namespace:        env.Namespace,
+			KubeconfigBase64: kubeconfigBase64,
+		}); err != nil {
+			log.Printf("error publishing: %v", err)
+		}
+	}
+	return nil
+}
+
+func (pa *ProjectApplication) GetProjectCluster(ctx context.Context, projectId int64, userId int64) (*value.ProjectCluster, error) {
 	cluster, err := pa.projectRepository.GetProjectCluster(ctx, projectId, userId)
 	if err != nil {
 		return nil, err
