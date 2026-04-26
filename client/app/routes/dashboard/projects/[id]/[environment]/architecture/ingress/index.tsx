92f353f6ab6fe17f89b8f4713cd93afed7c475db
Leon Liang <hello@leonliang.lu>
2026-04-26 22:11:52 +0200

    #221 give user full control over ingress host

--- client/app/routes/dashboard/projects/[id]/[environment]/architecture/ingress/index.tsx
+++ client/app/routes/dashboard/projects/[id]/[environment]/architecture/ingress/index.tsx
@@ -3,31 +3,21 @@ import DeployIngressForm, {
   type IngressFormInput,
 } from "~/components/organisms/forms/DeployIngressForm";
 import { useTRPC } from "~/utils/trpc/react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useEnvironment } from "~/routes/dashboard/projects/[id]/[environment]/architecture/layout";
-import { useOrganizationContext } from "~/contexts/OrganizationContext";
 
 export default function Index() {
   const trpc = useTRPC();
   const createIngressMutation = useMutation(
     trpc.deployment.deployIngress.mutationOptions(),
   );
-  const { environment: currentEnvironment, clusterId } = useEnvironment();
-  const { data: clusterData } = useQuery(
-    trpc.cluster.getCluster.queryOptions(
-      { id: clusterId! },
-      { enabled: !!clusterId },
-    ),
-  );
-
-  const organization = useOrganizationContext();
+  const { environment: currentEnvironment } = useEnvironment();
 
   const onSubmit = async (data: IngressFormInput) => {
     await createIngressMutation.mutateAsync({
       id: currentEnvironment.id,
       ingressHosts: data.hosts.map((h) => ({
-        host:
-          h.name + `.${organization.slug}.${clusterData?.ipv4Address}.nip.io`,
+        host: h.name,
         paths: h.paths.map((p) => ({
           path: p.path,
           pathType: p.pathType as "Prefix" | "Exact",
