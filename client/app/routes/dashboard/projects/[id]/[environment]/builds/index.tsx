2154c0c456e155773c4b8043a339aea9fe8653e6
Leon Liang <hello@leonliang.lu>
2026-05-09 21:36:29 +0200

    #130 improve no builds message

--- client/app/routes/dashboard/projects/[id]/[environment]/builds/index.tsx
+++ client/app/routes/dashboard/projects/[id]/[environment]/builds/index.tsx
@@ -2,9 +2,12 @@ import React, { useMemo } from "react";
 import BuildCard from "~/components/organisms/build-card/BuildCard";
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "~/utils/trpc/react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import { Box } from "lucide-react";
+import Button from "~/components/atoms/button/Button";
 
 export default function Builds() {
+  const navigate = useNavigate();
   const trpc = useTRPC();
   const { id, environment } = useParams<{
     id: string;
@@ -39,10 +42,36 @@ export default function Builds() {
 
   if (!isLoading && environmentBuilds?.length === 0)
     return (
-      <div className="flex flex-col gap-1 p-4">
-        <p className="text-mauve-11">
-          There are no builds for this environment yet.
-        </p>
+      <div className="flex h-full flex-col items-center justify-center gap-4 p-4">
+        <div className="bg-mauve-3 relative flex h-12 w-12 items-center justify-center rounded-sm">
+          {/* top left */}
+          <div className="border-mauve-8 absolute top-0 left-0 h-3 w-3 rounded-tl-sm border-t-2 border-l-2" />
+
+          {/* top right */}
+          <div className="border-mauve-8 absolute top-0 right-0 h-3 w-3 rounded-tr-sm border-t-2 border-r-2" />
+
+          {/* bottom left */}
+          <div className="border-mauve-8 absolute bottom-0 left-0 h-3 w-3 rounded-bl-sm border-b-2 border-l-2" />
+
+          {/* bottom right */}
+          <div className="border-mauve-8 absolute right-0 bottom-0 h-3 w-3 rounded-br-sm border-r-2 border-b-2" />
+
+          <Box className="text-mauve-9 h-8 w-8" />
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-mauve-12 text-sm">
+            There are no builds in this environment yet.
+          </p>
+          <p className="text-mauve-11 text-sm">
+            Deploy your first service and view the build logs here.
+          </p>
+        </div>
+        <Button
+          onClick={() => navigate("../architecture", { relative: "path" })}
+          className="w-32"
+        >
+          Deploy Service
+        </Button>
       </div>
     );
 
