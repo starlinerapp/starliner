be66550f01f949fe4bb3bd0a1e55391edb4302f5
Leon Liang <hello@leonliang.lu>
2026-05-16 10:11:56 +0200

    #279 move drizzle to auth service

--- auth/src/infrastructure/db/index.ts
+++ auth/src/infrastructure/db/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { serverEnv } from "~/env.server";
+import * as schema from "~/infrastructure/db/schema";
 
-export const db = drizzle(serverEnv.AUTH_DATABASE_URL);
+export const db = drizzle(serverEnv.AUTH_DATABASE_URL, { schema });
