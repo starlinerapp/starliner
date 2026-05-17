87ad7b74fb5cd0b0dafd96028ff199f7fa92e02d
Leon Liang <hello@leonliang.lu>
2026-05-17 22:29:17 +0200

    #285 Improve UI in teams page

--- client/app/components/organisms/settings/OrganizationTeams.tsx
+++ client/app/components/organisms/settings/OrganizationTeams.tsx
@@ -10,9 +10,15 @@ export default function OrganizationTeams() {
   const { slug } = useParams();
   const organization = useOrganizationContext();
 
-  const { data: teamsData, isLoading } = useQuery(
+  const { data: teamsData, isLoading: isTeamsLoading } = useQuery(
     trpc.team.getUserTeams.queryOptions({
       organizationId: organization.id,
+    }),
+  );
+
+  const { data: members, isLoading: isMembersLoading } = useQuery(
+    trpc.organization.getOrganizationMembers.queryOptions({
+      id: organization.id,
     }),
   );
 
@@ -22,7 +28,7 @@ export default function OrganizationTeams() {
         <div className="border-mauve-6 text-mauve-12 bg-gray-2 flex h-14 items-center justify-between border-b px-4 text-xs font-bold uppercase">
           <p>Your Teams</p>
         </div>
-        {isLoading ? (
+        {isTeamsLoading || isMembersLoading ? (
           <>
             {Array.from({ length: 3 }).map((_, i) => (
               <div
@@ -42,8 +48,17 @@ export default function OrganizationTeams() {
               to={`/${slug}/settings/teams/${team.id}`}
               className="border-mauve-6 text-mauve-12 hover:bg-gray-2 flex items-center justify-between border-b px-4 py-3 text-sm last:border-b-0"
             >
-              <div className="flex flex-col">
-                <span>#{team.slug}</span>
+              <div className="flex items-center gap-3">
+                <div className="bg-violet-9 flex h-9 w-9 items-center justify-center rounded-md text-base text-white">
+                  {team.slug.substring(0, 1)?.toUpperCase()}
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-violet-11">#{team.slug}</span>
+                  <p className="text-mauve-11 text-sm">
+                    {members?.length ?? 0}{" "}
+                    {(members?.length ?? 0) === 1 ? "Member" : "Members"}
+                  </p>
+                </div>
               </div>
             </Link>
           ))
