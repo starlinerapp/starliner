bf9f26b51dd8ad6709040483a522f73c568fb366
Leon Liang <hello@leonliang.lu>
2026-05-17 22:51:24 +0200

    #285 Fix Skeleton

--- client/app/components/organisms/settings/OrganizationTeams.tsx
+++ client/app/components/organisms/settings/OrganizationTeams.tsx
@@ -30,12 +30,18 @@ export default function OrganizationTeams() {
         </div>
         {isTeamsLoading || isMembersLoading ? (
           <>
-            {Array.from({ length: 3 }).map((_, i) => (
+            {Array.from({ length: 2 }).map((_, i) => (
               <div
                 key={i}
-                className="border-mauve-6 text-mauve-12 flex items-center justify-between border-b px-4 py-3 text-sm last:border-b-0"
+                className="border-mauve-6 flex items-center justify-between border-b px-4 py-3 text-sm last:border-b-0"
               >
-                <Skeleton className="h-5 w-24" />
+                <div className="flex items-center gap-3">
+                  <Skeleton className="h-9 w-9 rounded-md" />
+                  <div className="flex flex-col gap-1.5">
+                    <Skeleton className="h-4 w-20" />
+                    <Skeleton className="h-4 w-24" />
+                  </div>
+                </div>
               </div>
             ))}
           </>
