3182f0bc151fa72c8eb868ea892f6b910005c4d4
Leon Liang <hello@leonliang.lu>
2026-05-17 22:45:24 +0200

    #285 Fix Skeleton

--- client/app/components/organisms/settings/TeamMembers.tsx
+++ client/app/components/organisms/settings/TeamMembers.tsx
@@ -134,14 +134,17 @@ export default function TeamMembers({ teamId }: { teamId: number }) {
       </div>
       {isLoading ? (
         <>
-          {Array.from({ length: 3 }).map((_, i) => (
+          {Array.from({ length: 1 }).map((_, i) => (
             <div
               key={i}
-              className="border-mauve-6 text-mauve-12 flex items-center justify-between border-b px-4 py-2 text-sm last:border-b-0"
+              className="border-mauve-6 text-mauve-12 flex items-center justify-between border-b px-4 py-3 text-sm last:border-b-0"
             >
-              <div className="flex flex-col gap-1">
-                <Skeleton className="h-5 w-24" />
-                <Skeleton className="h-5 w-36" />
+              <div className="flex items-center gap-3">
+                <Skeleton className="h-8 w-8 rounded-full" />
+                <div className="flex flex-col gap-1">
+                  <Skeleton className="h-4.5 w-28" />
+                  <Skeleton className="h-4.5 w-44" />
+                </div>
               </div>
             </div>
           ))}
