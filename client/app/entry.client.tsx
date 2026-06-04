2dc21b206011ddf7467e01c5e610947c5267749e
Rafael Neher <rafineher@gmail.com>
2026-06-04 18:21:01 +0200

    remove 404 guards

--- client/app/entry.client.tsx
+++ client/app/entry.client.tsx
@@ -1,18 +1,12 @@
 import * as Sentry from "@sentry/react-router";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
-import { isRouteErrorResponse } from "react-router";
 import { HydratedRouter } from "react-router/dom";
 
 Sentry.init({
   dsn: window.ENV.SENTRY_DSN_CLIENT,
   environment: window.ENV.ENVIRONMENT,
   sendDefaultPii: true,
-  beforeSend(event, hint) {
-    const error = hint?.originalException;
-    if (isRouteErrorResponse(error) && error.status === 404) return null;
-    return event;
-  },
   integrations: [
     Sentry.reactRouterTracingIntegration(),
     Sentry.replayIntegration(),
