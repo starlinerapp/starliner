36391c951b6723094db90fea8d6bedb94578b95b
Leon Liang <hello@leonliang.lu>
2026-06-27 13:29:04 +0200

    #422 fix naming

--- server/cmd/builder/main.go
+++ server/cmd/builder/main.go
@@ -7,19 +7,19 @@ import (
 	"starliner.app/internal/builder/domain/service"
 	grpcClient "starliner.app/internal/builder/infrastructure/grpc"
 	"starliner.app/internal/builder/infrastructure/nats/impl/queue"
-	builderRedis "starliner.app/internal/builder/infrastructure/redis"
+	"starliner.app/internal/builder/infrastructure/redis"
 	grpcServer "starliner.app/internal/builder/presentation/grpc"
 	builderqueue "starliner.app/internal/builder/presentation/queue"
 	"starliner.app/internal/builder/presentation/scheduler"
-	"starliner.app/internal/core/infrastructure/redis"
+	redisClient "starliner.app/internal/core/infrastructure/redis"
 	"starliner.app/internal/core/infrastructure/sentry"
 )
 
 func main() {
 	fx.New(
 		conf.Module,
+		redisClient.Module,
 		redis.Module,
-		builderRedis.Module,
 		queue.Module,
 		grpcClient.Module,
 		application.Module,
